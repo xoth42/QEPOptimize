96c0ce9c0f37369fd0350f748da9d84e89cebe64
xoth42 <xoth42@protonmail.com>
2025-02-19 17:35:02 -0500

    small update for latest tasks

--- QEPO/test/optimizer_IBMTest.jl
+++ QEPO/test/optimizer_IBMTest.jl
@@ -11,11 +11,21 @@ config = Configuration(hw_cfg,adv_cfg)
 config
 using QEPO.Optimizer
 pop = Population()
-config.num_simulations = 10000
+config.num_simulations = 100
 config.max_gen = 20
 # initialize_pop_with_constraints!(pop,config) 
 run_with_constraints_history!(pop,config)
-
+config.optimize_for
 display_top_circuits(pop.individuals,5)
 pop.individuals[1]
+config.advanced_config.communication_fidelity_in
 # plot_performance_metrics(pop)
+config.advanced_config.population_size
+
+print(pop.individuals[1].ops)
+using QEPO.Optimizer: calculate_performance!
+calculate_performance!(pop.individuals[1],20,1,4,config.optimize_for,config.advanced_config)
+for i in 1:20
+    calculate_performance!(pop.individuals[i],20,1,4,config.optimize_for,config.advanced_config)
+
+end
