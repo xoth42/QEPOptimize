a5cb43b80334665169cdf2d3e0464693fe863a0a
Stefan Krastanov <stefan@krastanov.org>
2025-03-18 11:00:15 -0400

    scaffolding for optimization example

--- example/optimize.jl
+++ example/optimize.jl
@@ -1,4 +1,16 @@
 include("../src/QEPO.jl") # TODO once we have an actual library this will be just a `using` statement
 using .QEPO
 
+using .QEPO: initialize_pop! # TODO export these
+
 ##
+
+pop = Population()
+
+initialize_pop!(pop) # TODO do a 3-register run
+
+# TODO evolve for 100 generations
+
+# TODO plot history of the evolution (best, worst, etc fidelities over generations)
+
+# TODO plot F_in vs F_out and F_in vs P for the best circuits
