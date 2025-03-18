2f63472c48d6ea461ff95e49d8795d675d2b3ddf
Stefan Krastanov <stefan@krastanov.org>
2025-03-18 09:07:11 -0400

    initial cleanup of step! and related functions

---
+++ src/evolve.jl
@@ -0,0 +1,98 @@
+"""
+Important: This calls sort and cull.
+
+Execute one generation step of the genetic algorithm
+
+Each generation, the population is optimized by applying mutation, selection, and crossover operations
+while respecting constraints (such as gate connectivity and noise calibration)
+"""
+function step!(
+    population::Population;
+    max_ops::Int=5,
+    number_registers::Int=1,
+    purified_pairs::Int=1,
+    num_simulations::Int=100,
+    number_registers::Int=1,
+    pop_size::Int=100,
+)
+    # Mark existing individuals as survivors
+    # Survivors ensure that some individuals are carried over unchanged, maintaining good solutions
+    for indiv in population.individuals
+        indiv.history = :survivor
+    end
+
+    # TODO parents and offspring circuits
+
+    add_mutations!(population.individuals; max_ops, new_mutants, valid_pairs=1:number_registers) # TODO (low priority) decouple valid_pairs from number_registers
+
+    # Sort the population by fitness and cull the excess individuals to maintain the population size
+    simulate_and_sort!(
+        population;
+        num_simulations,
+        purified_pairs,
+        number_registers, # TODO (low priority) this should be by-default derived from `indiv`
+        noises=[NetworkFidelity(0.9)] # TODO configurable noise
+    )
+    cull!(population, pop_size)
+end
+
+"Adds mutated individuals to the given individual vector."
+function add_mutations!(
+    individuals::Vector{Individual};
+    max_ops::Int=5,
+    valid_pairs=1:1,
+    new_mutants::Int=10,
+    p_drop=0.1,
+    p_mutate=0.1,
+    p_gain=0.1,
+)
+    mutants = Individual[]
+
+    # For every mutation per individual, up to the limit
+    for _ in 1:new_mutants
+        l = length(indiv.ops)
+        drop_op::Bool   = rand() < p_drop   && l > 0
+        gain_op::Bool   = rand() < p_gain   && l < max_ops
+        # swap_op::Bool   = TODO
+        mutate_op::Bool = rand() < p_mutate && l > 0
+
+        drop_op && push!(mutants, drop_op(indiv))
+        gain_op && push!(mutants, gain_op(indiv; valid_pairs))
+        mutate_op && push!(mutants, mutate(indiv))
+    end
+
+    ## add all children back to the individuals vector
+    append!(individuals, mutants)
+end
+
+function sort_pop!(population::Population)
+    # update the population with the sorted vector of individuals by fitness
+    population.individuals = sort(population.individuals, by=indiv -> indiv.fitness, rev=true)
+end
+
+"Evaluate and Sort the individuals in descending order of fitness"
+function simulate_and_sort!(
+    population::Population;
+    num_simulations::Int=100,
+    purified_pairs::Int=1,
+    number_registers::Int=1, # TODO (low priority) this should be by-default derived from `indiv`
+    code_distance::Int=1,
+    noises=[NetworkFidelity(0.9)]
+)
+    # calculate and update each individual's performance
+    Threads.@threads for indiv in population.individuals
+        calculate_performance!(indiv,
+            num_simulations,
+            purified_pairs,
+            number_registers,
+            code_distance,
+            noises)
+        indiv.fitness = indiv.performance.purified_pairs_fidelity # TODO make it possible to select the type of fitness to evaluate
+    end
+    sort_pop!(population)
+end
+
+"Reduce the population size to the target `population_size` (assumes pop is already sorted)"
+function cull!(population::Population, population_size::Int)
+    population.individuals = population.individuals[1:population_size]
+end
